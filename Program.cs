ecfa1ee596b89ce817cbba6cca491a22ac053cd2
EmanuelBenitezBaez <95762072+EmanuelBenitezBaez@users.noreply.github.com>
2023-01-21 20:08:42 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿
+
+/*
+Ejercicio 1
+Crea una estructura de datos para un cliente con estos campos:
+Nombre completo
+Teléfono
+Dirección
+Email
+Si es nuevo cliente
+Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+*/
+
+
+
 Cliente cliente = new Cliente { NombreCompleto = "John Doe", Telefono = "555-555-5555", Direccion = "123 Main St", Email = "johndoe@example.com", EsNuevoCliente = true };
 cliente.MostrarDatos();
 
