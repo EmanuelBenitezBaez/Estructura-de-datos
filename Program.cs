9ee7a2b8dc8f2008b5aa1e3f1df4c401dbc5d426
EmanuelBenitezBaez <95762072+EmanuelBenitezBaez@users.noreply.github.com>
2023-01-21 20:07:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Cliente cliente = new Cliente { NombreCompleto = "John Doe", Telefono = "555-555-5555", Direccion = "123 Main St", Email = "johndoe@example.com", EsNuevoCliente = true };
+cliente.MostrarDatos();
+
+struct Cliente
+{
+    public string NombreCompleto;
+    public string Telefono;
+    public string Direccion;
+    public string Email;
+    public bool EsNuevoCliente;
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine("Nombre completo: " + NombreCompleto);
+        Console.WriteLine("Teléfono: " + Telefono);
+        Console.WriteLine("Dirección: " + Direccion);
+        Console.WriteLine("Email: " + Email);
+        Console.WriteLine("Es nuevo cliente: " + EsNuevoCliente);
+    }
+
+}
